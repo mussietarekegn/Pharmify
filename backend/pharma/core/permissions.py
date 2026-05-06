fe611bfd7f21c5dd1466f93764cc62dc8e7bd987
mussietarekegn <mussie.tarekegn@aastustudent.edu.et>
2026-05-06 13:12:44 +0300

    feat: enforce verified owner permissions and secure medicine creation with pharmacy ownership binding

---
+++ backend/pharma/core/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission
+
+class IsOwner(BasePermission): 
+    def has_permission(self, request, view):
+        return (request.user.is_authenticated and request.user.role == 'owner')
+
+class IsVerifiedOwner(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated and
+            request.user.role == 'owner' and
+            hasattr(request.user, 'pharmacy') and
+            request.user.pharmacy.is_verified
+        )
--- backend/pharma/core/views.py
+++ backend/pharma/core/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Medicine
 from .serializers import MedicineSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from .permissions import IsOwner,IsVerifiedOwner
 
 # Create your views here.
 
 class MedicineViewSet(viewsets.ModelViewSet):
     queryset = Medicine.objects.all()
     serializer_class = MedicineSerializer
-    permission_classes = [IsAuthenticated]
+    
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsVerifiedOwner()]
+        return [AllowAny()]
+    
+    def perform_create(self, serializer):
+        pharmacy = self.request.user.pharmacy
+        serializer.save(pharmacy=pharmacy)
+    
